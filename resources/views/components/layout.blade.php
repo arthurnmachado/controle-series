14c2788576c405f318cf345c02a5625ae0911e2e
arthurnmachado <fuginceas@gmail.com>
2024-04-04 17:54:42 -0400

    Aula 4 - Laravel Mix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - Controle de Séries</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1>{{ $title }}</h1>
+    <div class="container">
+        <h1>{{ $title }}</h1>
+        
+        {{ $slot }}
+    </div>
 
-{{ $slot }}
 </body>
 </html>
