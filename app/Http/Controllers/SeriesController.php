a4bf01f39f81d151eeeceeefe69ab00af22c9a29
arthurnmachado <fuginceas@gmail.com>
2024-04-11 18:49:30 -0400

    Voltando a seguir o projeto original do curso

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,40 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index() {
-        $series = Serie::query()->orderby('nome')->get();
+    public function index()
+    {
+        $series = Serie::query()->orderBy('nome')->get();
 
-        return view('series.index')
-            ->with('series', $series);
+        return view('series.index')->with('series', $series);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('series.create');
     }
 
-    public function store(Request $request) {
-        $nomeSerie = $request->input('nome');
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
+    public function store(Request $request)
+    {
+        Serie::create($request->all());
 
         return redirect('/series');
-    }
-
-    public function delete(Request $request) {
-        $deleteId = $request->input('id');
-        $serieDelete = Serie::find($deleteId);
-
-        if($serieDelete) {
-            $serieDelete->delete();
-            return response()->json(['message' => 'Item deletado com sucesso']);
-        }
-        else
-            return response()->json(['message' => 'Erro ao excluir a série'], 404);
     }
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,15 +1,9 @@
-<x-layout title='Séries'>
-    <a href="series/criar" class="btn btn-dark mb-2">Adicionar</a>
+<x-layout title="Séries">
+    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
         @foreach ($series as $serie)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                {{ $serie->nome }} 
-                <button type="button" class="btn btn-danger" onclick="deletarItem({{ $serie->id }}, '{{ csrf_token() }}')">
-                    <img src="{{ asset('media/icons/trash-icon.svg') }}" alt="Trash Icon" style="width: 20px;">
-                </button>                
-            </li>
-            
+        <li class="list-group-item">{{ $serie->nome }}</li>
         @endforeach
     </ul>
 </x-layout>
