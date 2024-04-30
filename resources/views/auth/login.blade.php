6e64284c1d33f5c8e50bebe081aeb5d66dc5c1c9
arthurnmachado <fuginceas@gmail.com>
2024-04-30 12:44:51 -0400

    ajustes no login + implementação de envio de emails

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,17 +40,23 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+            <div class="flex items-center justify-between mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+            
+                <div class="flex items-center justify-end">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+            
+                    <x-button class="ml-3">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
+            </div>            
         </form>
     </x-auth-card>
 </x-guest-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 bg-white border-b border-gray-200 text-center">
+                    <a class="underline text-gray-600 hover:text-gray-900" href="{{route('series.index')}}">
+                        Ir para a aplicação
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
+    </div>    
 </x-app-layout>
